a6b715689e72845cd82cb6fb73cb4f4950227f32
Krzysiek Kuczma <kuczma.krzysiek@gmail.com>
2017-07-05 16:59:08 +0200

    Zmieniony lauout, ujednolicona grafika.

--- js/script.js
+++ js/script.js
@@ -12,26 +12,24 @@
     var newStyle = document.querySelectorAll(".game");
     var winner = document.querySelector(".winner");
     var result = '';
-    console.log(newStyle);
       if (player1 === 1){
-          newStyle[0].style.backgroundPositionY = "0%"
+          newStyle[0].style.backgroundPositionY = "25%"
       } else if (player1 === 2) {
-          newStyle[0].style.backgroundPositionY = "33%"
+          newStyle[0].style.backgroundPositionY = "50%"
       } else if (player1 === 3) {
-          newStyle[0].style.backgroundPositionY = "66%"
+          newStyle[0].style.backgroundPositionY = "74%"
       }
       if (player2 === 1){
-          newStyle[1].style.backgroundPositionY = "0%"
+          newStyle[1].style.backgroundPositionY = "25%"
       } else if (player2 === 2) {
-          newStyle[1].style.backgroundPositionY = "33%"
+          newStyle[1].style.backgroundPositionY = "50%"
       } else if (player2 === 3) {
-          newStyle[1].style.backgroundPositionY = "66%"
+          newStyle[1].style.backgroundPositionY = "74%"
       }
        if (player1 === player2){
             for (var i = 0; i < newStyle.length; i++) {
                 newStyle[i].style.backgroundPosition = "bottom left";
                 result = "Remis"
-                console.log(result);
             }
        } else if ((player1 === 1 && player2 === 3) ||
           (player1 === 2 && player2 === 1) ||
@@ -39,12 +37,10 @@
             newStyle[0].style.backgroundPositionX = "left";
             newStyle[1].style.backgroundPositionX = "right";
             result = "Gracz 1 wygrał"
-           console.log(result);
        } else {
             newStyle[0].style.backgroundPositionX = "right";
             newStyle[1].style.backgroundPositionX = "left";
             result = "Gracz 2 wygrał"
-           console.log(result);
        }
     winner.innerHTML = ("<p>" + result + "</p>");
   };
